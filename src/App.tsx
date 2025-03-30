2d51990f4fe3b1a07235e9655a8b5535b8381928
Yash-KK <reachyashk@gmail.com>
2025-03-30 19:21:36 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen bg-gray-200 text-4xl font-bold ">
+        Inital Setup
+      </div>
+    </>
+  );
+}
+
+export default App;
